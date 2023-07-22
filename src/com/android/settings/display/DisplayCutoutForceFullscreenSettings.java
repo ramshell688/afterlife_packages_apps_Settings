8c69d07cff89c4c31098ff5d7fced29b90a7cfe1
ashwani212 <querytechyasJ7@gmail.com>
2023-07-22 11:45:08 +0000

    Revert "Settings: Use new material-you illustration preference"
    This reverts commit 3c684318a44350909f768ad2320c44350a003c27.
    
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- src/com/android/settings/display/DisplayCutoutForceFullscreenSettings.java
+++ src/com/android/settings/display/DisplayCutoutForceFullscreenSettings.java
@@ -371,6 +371,6 @@ public class DisplayCutoutForceFullscreenSettings extends SettingsPreferenceFrag
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CUSTOMIZE;
+        return MetricsEvent.AFTERLIFE;
     }
 }
