258e1e92a1060aae88d34e13b066bc3a4bf6be3a
Aditya Eka Pranata <aepranata@gmail.com>
2023-11-02 06:20:51 +0700

    Fixup miss merge d73ee73
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- src/com/android/settings/localepicker/AppLocalePickerActivity.java
+++ src/com/android/settings/localepicker/AppLocalePickerActivity.java
@@ -66,11 +66,6 @@ public class AppLocalePickerActivity extends SettingsBaseActivity
             finish();
             return;
         }
-        if (!canDisplayLocaleUi() || mContextAsUser.getUserId() != UserHandle.myUserId()) {
-            Log.w(TAG, "Not allow to display Locale Settings UI.");
-            finish();
-            return;
-        }
 
         setTitle(R.string.app_locale_picker_title);
         getActionBar().setDisplayHomeAsUpEnabled(true);
